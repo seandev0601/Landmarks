7b2420609b3a7890fbedd4293348f145ed0cddac
Sean <seanbopo@gmail.com>
2023-06-17 10:42:34 +0800

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Po Hsiang Chao on 2023/6/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Po Hsiang Chao on 2023/6/17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
